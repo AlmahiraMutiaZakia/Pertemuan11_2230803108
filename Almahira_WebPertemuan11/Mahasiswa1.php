add63db726a70a476ceb94abdb98f28ede2bb7e2
AlmahiraMutiaZakia <144992951+AlmahiraMutiaZakia@users.noreply.github.com>
2023-11-16 15:49:16 +0700

    Add files via upload

---
+++ Almahira_WebPertemuan11/Mahasiswa1.php
@@ -0,0 +1,19 @@
+<?php
+$mahasiswa =["Joni","123456","Sistem Informasi","almahiramutia11@gmail.com"]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>mahasiswa</title>
+</head>
+<body>
+<h1>Daftar Mahasiswa</h1>
+<ul>
+    <li>Joni</li>
+    <li>123456</li>
+    <li>Sistem Informasi</li>
+    <li>almahiramutia11@gmail.com</li>
+</ul>
+    
+</body>
+</html>
